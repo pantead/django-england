6f9f444898f6bf9c8d89b11755bd5ee888a3e57a
pantead <87699466+pantead@users.noreply.github.com>
2021-11-20 16:07:03 +0330

    urls.py
    accounts

--- urls.py
+++ urls.py
@@ -1,27 +1,35 @@
-"""tutorial URL Configuration
+from django.conf.urls import url
+from django.contrib.auth.forms import PasswordResetForm
+from django.contrib.auth.views import LoginView, LogoutView
+# from django.contrib.auth.views import LoginView, LogoutView, PasswordResetCompleteView, PasswordResetDoneView
+from accounts import views
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.conf.urls import include, url
-from django.contrib import admin
-from tutorial import views
-    
+
+
+
 urlpatterns = [
-    url(r'^$', views.login_redirect, name='login_redirect'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^account/', include('accounts.urls'))
-]
+    url(r'^$',views.home),
+    url(r'^login/$', LoginView.as_view(template_name='accounts/loginview.html'), name='login'),
+    url(r'^logout/$', LogoutView.as_view(template_name='accounts/logoutview.html'), name='logout'), 
+    url(r'^register/$',views.register, name ='register'),
+    # url(r'^profile/$',views.view_profile, name ='view_profile'),
+    # url(r'^profile/edit/$',views.edit_profile , name ='edit profile'),
+    url(r'^change-password/$',views.change_password, name ='change_password')
 
+    # url(r'^reset-password/$', PasswordResetForm, {'template_name':
+    # 'accounts/reset_password.html', 'post_reset_redirect':
+    # 'accounts: password_reset_done', 'email_template_name':
+    # 'accounts/reset_password_email.html'},name ='reset_password'),
+
+
+    # url(r'^reset-password/done/$', PasswordResetDoneView, {'template_name':
+    # 'accounts/reset_password_done.html'},name ='reset_password_done'),
+
+    # url(r'^reset-password/complete/$',PasswordResetCompleteView,{'template_name':
+    # 'accounts/reset_password_complete.html'},name ='password_reset_complete')
+  
+
+    
+]
 
 
